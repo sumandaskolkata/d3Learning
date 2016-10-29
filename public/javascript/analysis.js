cdf0f9a2dc20761c9d54c74d3a657bed2e525f4c
sumandaskolkata <sumandas.kolkata@yahoo.com>
2016-10-29 11:40:59 +0530

    every one second a new data is coming and bar chart is updating according to the data.

--- public/javascript/analysis.js
+++ public/javascript/analysis.js
@@ -1,16 +1,30 @@
 var _DATA = [40,18,58,59,58,12,20,88,76,56];
 
 var createBarChart = function(){
-    console.log(_DATA)
     d3.select(".container").append("div").classed("firstChart",true);
-
+    _DATA.shift();
+    _DATA.push(_.random(1,100))
      d3.select(".firstChart").selectAll("div")
         .data(_DATA)
         .enter().append("div")
         .style("width",function(d){ return d*4+"px" })
         .style("height","25px")
-        .style("margin-bottom","2px")
-        .classed("bar",true);   
+        .style("margin-bottom","1px")
+        .style("margin-top","1px")
+        .classed("bar",true); 
+    setInterval(updateChart,1000);
+        
 }
 
+var updateChart = function(){
+    _DATA.shift();
+    _DATA.push(_.random(1,100))
+     var bars = d3.select(".firstChart").selectAll("div")
+        .data(_DATA)
+
+        bars.style("width",function(d){ return d*4+"px" })
+        .style("height","25px")
+        .style("margin-bottom","2px")
+        .classed("bar",true); 
+}
 window.onload = createBarChart;
